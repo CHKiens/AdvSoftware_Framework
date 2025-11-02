2a831d2185902cf4e1fe59b0560ede7e344d0489
CHKiens <44415938+CHKiens@users.noreply.github.com>
2025-11-02 02:28:08 +0100

    Added sealed logger class

---
+++ Mandatory2DGameFramework/logger/MyLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Mandatory2DGameFramework.logger
+{
+    public sealed class MyLogger
+    {
+        private static readonly MyLogger instance = new MyLogger();
+        public static MyLogger Instance { get { return instance; } }
+
+        private TraceSource ts = new TraceSource("GameLog");
+
+        private MyLogger() { }
+
+        public void LogInfo(string message)
+        {
+            ts.TraceEvent(TraceEventType.Information, 0, message);
+            ts.Flush();
+        }
+
+        public void LogWarning(string message)
+        {
+            ts.TraceEvent(TraceEventType.Warning, 0, message);
+            ts.Flush();
+        }
+
+        public void LogError(string message)
+        {
+            ts.TraceEvent(TraceEventType.Error, 0, message);
+            ts.Flush();
+        }
+
+        public void LogCritical(string message)
+        {
+            ts.TraceEvent(TraceEventType.Critical, 0, message);
+            ts.Flush();
+        }
+    }
+}
